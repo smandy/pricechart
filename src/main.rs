aee6a7d688ac7ab851941c9394954f8464541f42
Andy Smith <andyrsmith@gmail.com>
2020-12-28 22:33:41 +0000

    Incremental.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,90 @@
-use rand::Rng;
+#![feature(allocator_api)]
 
+use rand::prelude::SliceRandom;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use sdl2::rect::{Point, Rect};
+use sdl2::render::TextureQuery;
+use std::alloc::Global;
+use std::path::Path;
+
+const WIDTH: u32 = 960;
+const HEIGHT: u32 = 960;
+
+macro_rules! rect(
+    ($x:expr, $y:expr, $w:expr, $h:expr) => (
+        Rect::new($x as i32, $y as i32, $w as u32, $h as u32)
+    )
+);
+
+#[derive(Debug, Clone)]
 struct OHLC {
-    open : f64,
-    high : f64,
-    low : f64,
-    close : f64
+    open: f64,
+    high: f64,
+    low: f64,
+    close: f64,
 }
 
-fn main() {
+trait RecvPrice<T> {
+    fn apply(&mut self, px: f64);
+}
+
+impl RecvPrice<f64> for Option<OHLC> {
+    fn apply(&mut self, px: f64) {
+        match self {
+            None => {
+                *self = Some(OHLC {
+                    open: px,
+                    high: px,
+                    low: px,
+                    close: px,
+                })
+            }
+            Some(x) => {
+                if px > x.high {
+                    x.high = px
+                }
+                if px < x.low {
+                    x.low = px
+                }
+                x.close = px;
+            }
+        }
+    }
+}
+
+fn get_centered_rect(rect_width: u32, rect_height: u32, cons_width: u32, cons_height: u32) -> Rect {
+    let wr = rect_width as f32 / cons_width as f32;
+    let hr = rect_height as f32 / cons_height as f32;
+
+    let (w, h) = if wr > 1f32 || hr > 1f32 {
+        if wr > hr {
+            println!("Scaling down! The text will look worse!");
+            let h = (rect_height as f32 / wr) as i32;
+            (cons_width as i32, h)
+        } else {
+            println!("Scaling down! The text will look worse!");
+            let w = (rect_width as f32 / hr) as i32;
+            (w, cons_height as i32)
+        }
+    } else {
+        (rect_width as i32, rect_height as i32)
+    };
+
+    let cx = (WIDTH as i32 - w) / 2;
+    let cy = (HEIGHT as i32 - h) / 2;
+    rect!(cx, cy, w, h)
+}
+
+fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
-    let ev = sdl_context.event().unwrap();
+    //let ev = sdl_context.event().unwrap();
+    let ttf_context = sdl2::ttf::init().map_err(|e| e.to_string())?;
     let video_subsystem = sdl_context.video()?;
+
     let window = video_subsystem
-        .window("A Knights Tour", 960, 960)
+        .window("Price chart", WIDTH, HEIGHT)
         .position_centered()
         .build()
         .map_err(|e| e.to_string())?;
@@ -21,16 +93,200 @@ fn main() {
         .software()
         .build()
         .map_err(|e| e.to_string())?;
+    let (all_prices, prices) = make_prices();
 
-    makePrices();
+    println!("allprices is {:?}", all_prices);
+    println!(
+        "prices is {:#?} {:#?}",
+        &prices[..5],
+        &prices[prices.len() - 5..]
+    );
+
+    let mut last_event = None;
+
+    'mainloop: loop {
+        for event in sdl_context.event_pump()?.poll_iter() {
+            match event {
+                Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                }
+                | Event::Quit { .. } => break 'mainloop,
+                /*                Event::User { .. } => {
+                                    //println!("Woot have a solution {:?}", event);
+                                }
+                */
+                _ => {
+                    //    println!("event is {:?}", event);
+                }
+            }
+            last_event = Some(event)
+        }
+
+        canvas.set_draw_color(Color::RGBA(0, 0, 0, 255));
+        canvas.clear();
+
+        let white = Color::RGBA(255, 255, 255, 255);
+        let red = Color::RGBA(255, 0, 0, 255);
+        let green = Color::RGBA(0, 255, 0, 255);
+
+        let scale_x = |x: i32| {
+            let f = (x as f64) / (prices.len() as f64);
+            return (f * WIDTH as f64) as i32;
+        };
+
+        let half_tickwidth = {
+            let ret = (0.45 * WIDTH as f64) / prices.len() as f64;
+            ret as u32
+        };
+
+        let OHLC {
+            high: all_high,
+            low: all_low,
+            ..
+        } = all_prices.clone().unwrap();
+
+        let yrange = all_high - all_low;
+
+        let mut font =
+            ttf_context.load_font(Path::new("/usr/share/fonts/JetBrainsMono-Medium.ttf"), 12)?;
+        let texture_creator = canvas.texture_creator();
+
+        let mut draw_string = |x, y, s| -> Result<(), String> {
+            let surface = font
+                .render(s)
+                .blended(Color::RGBA(0, 255, 255, 255))
+                .map_err(|e| e.to_string())
+                .unwrap();
+            let texture = texture_creator
+                .create_texture_from_surface(&surface)
+                .map_err(|e| e.to_string())
+                .unwrap();
+            let TextureQuery { width, height, .. } = texture.query();
+            //println("Query ")
+            canvas
+                .copy(&texture, None, Some(rect!(x, y, width, height)))
+                .unwrap();
+            Ok(())
+        };
+
+        let s1 = &std::format!("ah={} al={}", all_high, all_low);
+        let s2 = &std::format!("yrange={}", yrange);
+        draw_string(20, 20, s1);
+        draw_string(20, 40, s2);
+
+        if let Some(event) = &last_event {
+            //println!("Last event is {:#?}", event);
+            let s = &std::format!("evt is {:#?}", event);
+            draw_string(20, 60, s);
+        }
+
+        for tmp in &prices {
+            if let (
+                i,
+                Some(OHLC {
+                    open,
+                    high,
+                    low,
+                    close,
+                }),
+            ) = tmp
+            {
+                let scale_y = |px: f64| {
+                    //let range = all_high - all_how;
+                    let f = (all_high - px) / yrange;
+                    let ret = (f * (HEIGHT as f64)) as i32;
+                    //println!("scale {} -> {}", px, ret);
+                    return ret;
+                    //low + dif * range;
+                };
+                let x = scale_x(*i);
+                let o = scale_y(*open);
+                let h = scale_y(*high);
+                let l = scale_y(*low);
+                let c = scale_y(*close);
+                let s = Point::new(x, l);
+                let f = Point::new(x, h);
+                /*            println!(
+                    "s={:?} f={:?} tmp={:?} ah={} al={} yrange={}",
+                    s, f, tmp, all_high, all_low, yrange
+                );*/
+
+                canvas.set_draw_color(white);
+                canvas.draw_line(s, f);
+
+                {
+                    let rect = Rect::new(
+                        x - half_tickwidth as i32,
+                        o,
+                        2 * half_tickwidth,
+                        (c - o).abs() as u32,
+                    );
+                    canvas.set_draw_color(if close > open { green } else { red });
+                    canvas.fill_rect(rect)?;
+                    canvas.set_draw_color(white);
+                    canvas.draw_rect(rect);
+                }
+            }
+        }
+
+        let padding = 64;
+
+        canvas.present();
+    }
+
+    Ok(())
 }
 
-fn makePrices() {
+fn make_prices() -> (Option<OHLC>, Vec<(i32, Option<OHLC>), Global>) {
     let mut price = 100.0;
     let mut rng = rand::thread_rng();
+    let mut prices = Vec::new();
 
-    for i in 0..1_000_000 {
-        price *= rng.gen_range(0.9, 1.1);
-        println!("Price is {}", price);
+    let mut ohlc: Option<OHLC> = None;
+    let mut all = None;
+    println!("Ohlc is {:?}", ohlc);
+    let mut tix = 0;
+    let mut bar = 0;
+
+    let ticks = &[-0.01, -0.01, 0.0, 0.0, 0.0, 0.01, 0.01];
+
+    //impl Eq for f64 {}
+
+    fn min(x: f64, y: f64) -> f64 {
+        if x < y {
+            x
+        } else {
+            y
+        }
     }
+
+    fn max(x: f64, y: f64) -> f64 {
+        if x > y {
+            x
+        } else {
+            y
+        }
+    }
+
+    let mut tix = 0;
+    loop {
+        tix += 1;
+        if tix > 500 {
+            ohlc.apply(price);
+        }
+        all.apply(price);
+        let new_p = price + ticks.choose(&mut rng).unwrap();
+        price = max(10.0, new_p);
+        if tix == 10000 {
+            tix = 0;
+            bar += 1;
+            prices.push((bar, ohlc));
+            ohlc = None;
+            if prices.len() == 50 {
+                return (all, prices);
+            }
+        }
+    }
+    //return (all, prices);
 }
