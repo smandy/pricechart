728a9edd0f5cca05d6ed2a9a22d5f927aa11c24a
Andy Smith <andyrsmith@gmail.com>
2020-12-31 00:12:44 +0000

    ropey zoom/pan.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 use rand::prelude::SliceRandom;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::mouse::MouseButton;
 use sdl2::pixels::Color;
 use sdl2::rect::{Point, Rect};
 use sdl2::render::TextureQuery;
@@ -11,6 +12,7 @@ use std::path::Path;
 
 const WIDTH: u32 = 960;
 const HEIGHT: u32 = 960;
+const ZOOM_FACT: f64 = 20.0;
 
 macro_rules! rect(
     ($x:expr, $y:expr, $w:expr, $h:expr) => (
@@ -72,14 +74,53 @@ fn main() -> Result<(), String> {
         .map_err(|e| e.to_string())?;
     let (all_prices, prices) = make_prices();
 
-    /*    println!("allprices is {:?}", all_prices);
-    println!(
-        "prices is {:#?} {:#?}",
-        &prices[..5],
-        &prices[prices.len() - 5..]
-    );*/
-
     let mut last_event = None;
+    let mut zoomx = ZOOM_FACT;
+    let mut zoomy = ZOOM_FACT;
+    let OHLC {
+        high: all_high,
+        low: all_low,
+        ..
+    } = all_prices.clone().unwrap();
+
+    let mut cx: f64 = prices.len() as f64 / 2.0;
+    let mut cy: f64 = (all_high + all_low) / 2.0;
+
+    let mut dx: i32 = 0;
+    let mut dy: i32 = 0;
+
+    #[derive(Debug)]
+    enum MouseState {
+        Up,
+        Zooming {
+            x: i32,
+            y: i32,
+            initial_zoom_x: f64,
+            initial_zoom_y: f64,
+        },
+        Panning {
+            x: i32,
+            y: i32,
+        },
+    }
+
+    impl MouseState {
+        fn is_zoom(&self) -> bool {
+            match self {
+                MouseState::Zooming { .. } => true,
+                _ => false,
+            }
+        }
+
+        fn is_pan(&self) -> bool {
+            match self {
+                MouseState::Panning { .. } => true,
+                _ => false,
+            }
+        }
+    }
+
+    let mut mouse_state = MouseState::Up;
 
     'mainloop: loop {
         for event in sdl_context.event_pump()?.poll_iter() {
@@ -89,10 +130,54 @@ fn main() -> Result<(), String> {
                     ..
                 }
                 | Event::Quit { .. } => break 'mainloop,
-                /*                Event::User { .. } => {
-                                    //println!("Woot have a solution {:?}", event);
-                                }
-                */
+                Event::MouseMotion { x, y, .. } => match mouse_state {
+                    MouseState::Zooming {
+                        x: cx,
+                        y: cy,
+                        initial_zoom_x,
+                        initial_zoom_y,
+                    } => {
+                        zoomx = initial_zoom_x + 10.0 * (x as f64 - cx as f64) / WIDTH as f64;
+                        zoomy = initial_zoom_y + 10.0 * (y as f64 - cy as f64) / HEIGHT as f64;
+                    }
+                    MouseState::Panning { x: px, y: py } => {
+                        dx = x - px;
+                        dy = y - py;
+                    }
+                    MouseState::Up => {}
+                },
+                Event::MouseButtonUp {
+                    mouse_btn: MouseButton::Left,
+                    ..
+                } if mouse_state.is_pan() => mouse_state = MouseState::Up,
+                Event::MouseButtonUp {
+                    mouse_btn: MouseButton::Right,
+                    ..
+                } if mouse_state.is_zoom() => mouse_state = MouseState::Up,
+                Event::MouseButtonDown {
+                    mouse_btn: MouseButton::Right,
+                    x,
+                    y,
+                    ..
+                } => {
+                    mouse_state = MouseState::Zooming {
+                        x,
+                        y,
+                        initial_zoom_x: zoomx,
+                        initial_zoom_y: zoomy,
+                    }
+                }
+                Event::MouseButtonDown {
+                    mouse_btn: MouseButton::Left,
+                    x,
+                    y,
+                    ..
+                } => {
+                    mouse_state = MouseState::Panning {
+                        x: x - dx,
+                        y: y - dy,
+                    }
+                }
                 _ => {
                     //    println!("event is {:?}", event);
                 }
@@ -104,59 +189,19 @@ fn main() -> Result<(), String> {
         canvas.clear();
 
         let white = Color::RGBA(255, 255, 255, 255);
-        let red = Color::RGBA(255, 0, 0, 255);
-        let green = Color::RGBA(0, 255, 0, 255);
-
-        let scale_x = |x: i32| {
-            let f = (x as f64) / (prices.len() as f64);
-            return (f * WIDTH as f64) as i32;
-        };
+        let red = Color::RGBA(200, 0, 0, 255);
+        let green = Color::RGBA(0, 150, 0, 255);
 
         let half_tickwidth = {
-            let ret = (0.45 * WIDTH as f64) / prices.len() as f64;
+            let ret = (zoomx * 0.45 * WIDTH as f64) / prices.len() as f64;
             ret as u32
         };
 
-        let OHLC {
-            high: all_high,
-            low: all_low,
-            ..
-        } = all_prices.clone().unwrap();
-
-        let yrange = all_high - all_low;
+        //let yrange = all_high - all_low;
 
         let font =
             ttf_context.load_font(Path::new("/usr/share/fonts/JetBrainsMono-Medium.ttf"), 12)?;
         let texture_creator = canvas.texture_creator();
-
-        let mut draw_string = |x, y, s| -> Result<(), String> {
-            let surface = font
-                .render(s)
-                .blended(Color::RGBA(0, 255, 255, 255))
-                .map_err(|e| e.to_string())
-                .unwrap();
-            let texture = texture_creator
-                .create_texture_from_surface(&surface)
-                .map_err(|e| e.to_string())
-                .unwrap();
-            let TextureQuery { width, height, .. } = texture.query();
-            //println("Query ")
-            canvas
-                .copy(&texture, None, Some(rect!(x, y, width, height)))
-                .unwrap();
-            Ok(())
-        };
-
-        let s1 = &std::format!("ah={} al={}", all_high, all_low);
-        let s2 = &std::format!("yrange={}", yrange);
-        draw_string(20, 20, s1)?;
-        draw_string(20, 40, s2)?;
-
-        if let Some(event) = &last_event {
-            //println!("Last event is {:#?}", event);
-            let s = &std::format!("evt is {:#?}", event);
-            draw_string(20, 60, s)?;
-        }
 
         for tmp in &prices {
             if let (
@@ -169,11 +214,25 @@ fn main() -> Result<(), String> {
                 }),
             ) = tmp
             {
+                let scale_x = |x: i32| {
+                    //let cx = prices.len() as f64 / 2.0;
+                    let rx = prices.len() as f64;
+                    let ox = (x as f64 - cx) / rx;
+                    let fw = WIDTH as f64;
+                    let ret = dx + ((fw / 2.0) + fw * ox * zoomx) as i32;
+                    //return (f * WIDTH as f64) as i32;
+                    return ret;
+                };
+
                 let scale_y = |px: f64| {
                     //let range = all_high - all_how;
-                    let f = (all_high - px) / yrange;
-                    let ret = (f * (HEIGHT as f64)) as i32;
-                    //println!("scale {} -> {}", px, ret);
+                    let ry = all_high - all_low;
+                    //let cy = (all_high + all_low) / 2.0;
+                    let oy = (px - cy) / ry;
+                    //let f = (all_high - px) / yrange;
+                    let ih = HEIGHT as f64;
+                    let ret = dy + ((ih / 2.0) + ih * oy * zoomy) as i32;
+                    //println!("scale {} -> {} oy={} cy={}", px, ret, oy, cy);
                     return ret;
                     //low + dif * range;
                 };
@@ -184,14 +243,8 @@ fn main() -> Result<(), String> {
                 let c = scale_y(*close);
                 let s = Point::new(x, l);
                 let f = Point::new(x, h);
-                /*            println!(
-                    "s={:?} f={:?} tmp={:?} ah={} al={} yrange={}",
-                    s, f, tmp, all_high, all_low, yrange
-                );*/
-
                 canvas.set_draw_color(white);
                 canvas.draw_line(s, f)?;
-
                 {
                     let rect = Rect::new(
                         x - half_tickwidth as i32,
@@ -206,6 +259,41 @@ fn main() -> Result<(), String> {
                 }
             }
         }
+
+        let mut draw_string = |x, y, s: &String| -> Result<(), String> {
+            let surface = font
+                .render(s)
+                .blended_wrapped(Color::RGBA(200, 0, 255, 255), 200)
+                .map_err(|e| e.to_string())
+                .unwrap();
+            let texture = texture_creator
+                .create_texture_from_surface(&surface)
+                .map_err(|e| e.to_string())
+                .unwrap();
+            let TextureQuery { width, height, .. } = texture.query();
+            canvas
+                .copy(&texture, None, Some(rect!(x, y, width, height)))
+                .unwrap();
+            Ok(())
+        };
+
+        let s1 = &std::format!("ah={} al={}", all_high, all_low);
+        //let s2 = &std::format!("yrange={}", yrange);
+        draw_string(20, 20, s1)?;
+        //draw_string(20, 40, s2)?;
+
+        if let Some(event) = &last_event {
+            //println!("Last event is {:#?}", event);
+            let s3 = &std::format!("evt is {:#?}", *event);
+            draw_string(20, 60, s3)?;
+        }
+        //let s4 =
+        let s5 = &std::format!("zoomx={} zoomy={}", zoomx, zoomy);
+        draw_string(500, 20, s5)?;
+
+        let s6 = &std::format!("mouse_state={:?}", mouse_state);
+        draw_string(500, 40, s6)?;
+
         canvas.present();
     }
 
